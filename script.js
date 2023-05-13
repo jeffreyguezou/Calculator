0cb19b68456bf3627f4d1edd1669e6c6d853e28c
Jeffrey Raj <jeffreyrajcj@gmail.com>
2023-05-13 12:28:23 +0530

    Added calculator functionality for int numbers, floats pending

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
 const upperDisplay =  document.querySelector("#upperDisplay");
 const lowerDisplay = document.querySelector("#lowerDisplay");
-const buttons = document.querySelectorAll(".calcButton");
-let lowerNumber =  0;
-let upperNumber=0;
+const numberButtons = document.querySelectorAll(".numberButton");
+const operatorButtons = document.querySelectorAll(".operatorButton");
+const equalButton = document.querySelector(".calcButtonEqual");
+const acButton = document.querySelector("#acButton");
+const clearButton = document.querySelector("#clearButton");
+let operandOne;
+let upperText;
 
-function updateDisplay(){
-    lowerDisplay.textContent = lowerNumber;
+let lowerNumber = '';
+let upperNumber='';
+
+function addition(a,b){
+  lowerNumber = parseInt(a)+parseInt(b);
+  lowerDisplay.textContent= lowerNumber;
+  upperDisplay.textContent='';
+}
+function subtraction(a,b){
+  lowerNumber = a-b;
+  lowerDisplay.textContent= lowerNumber;
+  upperDisplay.textContent='';
+}
+function multiplication(a,b){
+  lowerNumber = a*b;
+  lowerDisplay.textContent= lowerNumber;
+  upperDisplay.textContent='';
+}
+function division(a,b){
+  lowerNumber = a/b;
+  lowerDisplay.textContent= lowerNumber;
+  upperDisplay.textContent='';
+}
+function operate(a,b,c){
+  if (c=== '+') addition(a,b);
+  if (c === '-' ) subtraction(a,b);
+  if( c === '*') multiplication(a,b);
+  if(c=== '/') division(a,b);
+
 }
 
-buttons.forEach((button)=>{
+numberButtons.forEach((button)=>{
    button.addEventListener("click",(e)=>{
-    lowerNumber=e.target.value;
-    updateDisplay();
-    console.log(lowerNumber);
-    console.log(e.target.value);
+    lowerNumber = lowerNumber+`${e.target.value}`;
+    lowerDisplay.textContent = lowerNumber;
    })
+   
  })
+
+ operatorButtons.forEach((operator)=>{
+  operator.addEventListener("click",(e)=>{
+    upperText = lowerNumber+`${e.target.value}`;
+    upperDisplay.textContent = upperText;
+    lowerDisplay.textContent='';
+    operandOne =  lowerNumber;
+    lowerNumber='';
+  })
+ })
+
+ equalButton.addEventListener("click",()=>{
+    operate(operandOne,lowerNumber,upperText.at(-1));
+ })
+acButton.addEventListener("click",()=>{
+  upperText='';
+  lowerNumber='';
+  upperNumber='';
+  lowerDisplay.textContent = lowerNumber;
+  upperDisplay.textContent = upperNumber;
+
+})
