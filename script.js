2d541993036884efa3a9f11e6dc55bd66e5a03ae
Jeffrey Raj <jeffreyrajcj@gmail.com>
2023-05-17 20:42:01 +0530

    Added functionality to chain operations, All clear button and backspace button

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 const upperDisplay =  document.querySelector("#upperDisplay");
 const lowerDisplay = document.querySelector("#lowerDisplay");
+const operatorDisplay = document.querySelector("#operator")
 const numberButtons = document.querySelectorAll(".numberButton");
 const operatorButtons = document.querySelectorAll(".operatorButton");
 const equalButton = document.querySelector(".calcButtonEqual");
 const acButton = document.querySelector("#acButton");
 const clearButton = document.querySelector("#clearButton");
-let operandOne;
+let operatorValue;
 let upperText;
 
 let lowerNumber = '';
 let upperNumber='';
 
 function addition(a,b){
-  lowerNumber = parseInt(a)+parseInt(b);
-  lowerDisplay.textContent= lowerNumber;
-  upperDisplay.textContent='';
+  return (parseFloat(a)+parseFloat(b));
+  
 }
 function subtraction(a,b){
-  lowerNumber = a-b;
-  lowerDisplay.textContent= lowerNumber;
-  upperDisplay.textContent='';
+  return(parseFloat(a)-parseFloat(b));
+ 
 }
 function multiplication(a,b){
-  lowerNumber = a*b;
-  lowerDisplay.textContent= lowerNumber;
-  upperDisplay.textContent='';
+  return(parseFloat(a)*parseFloat(b));
+  
 }
 function division(a,b){
-  lowerNumber = a/b;
-  lowerDisplay.textContent= lowerNumber;
-  upperDisplay.textContent='';
+  return (parseFloat(a)/parseFloat(b));
+  
 }
 function operate(a,b,c){
-  if (c=== '+') addition(a,b);
-  if (c === '-' ) subtraction(a,b);
-  if( c === '*') multiplication(a,b);
-  if(c=== '/') division(a,b);
+  if (c=== '+') return(addition(a,b));
+  if (c === '-' ) return (subtraction(a,b));
+  if( c === '*') return multiplication(a,b);
+  if(c=== '/') return division(a,b);
 
 }
 
@@ -49,22 +46,53 @@ numberButtons.forEach((button)=>{
 
  operatorButtons.forEach((operator)=>{
   operator.addEventListener("click",(e)=>{
-    upperText = lowerNumber+`${e.target.value}`;
-    upperDisplay.textContent = upperText;
-    lowerDisplay.textContent='';
-    operandOne =  lowerNumber;
-    lowerNumber='';
+    
+    if(upperNumber !=='' && lowerNumber !='' && operatorValue!='')
+    {
+      
+      upperNumber =  operate(upperNumber,lowerNumber,operatorValue);
+      upperNumber = roundTo5decimalsMax(upperNumber);
+      operatorValue = e.target.value;
+      operatorDisplay.textContent =  operatorValue;
+      upperDisplay.textContent= upperNumber;
+      lowerNumber = '';
+      lowerDisplay.textContent =  lowerNumber;
+    }
+    else{
+
+      operatorValue = e.target.value;
+      operatorDisplay.textContent =  operatorValue;
+      upperNumber =  lowerNumber;
+      upperDisplay.textContent =  upperNumber;
+      lowerNumber = '';
+      lowerDisplay.textContent =  lowerNumber;
+    }
+    
   })
  })
 
  equalButton.addEventListener("click",()=>{
-    operate(operandOne,lowerNumber,upperText.at(-1));
+  if(upperNumber && lowerNumber && operatorValue){
+    lowerNumber = operate(upperNumber,lowerNumber,operatorValue);
+    lowerNumber =  roundTo5decimalsMax(lowerNumber);
+    lowerDisplay.textContent =  lowerNumber;
+    upperNumber='';
+    upperDisplay.textContent = '';
+    operatorDisplay.textContent = '';
+  }
+    
  })
 acButton.addEventListener("click",()=>{
-  upperText='';
-  lowerNumber='';
-  upperNumber='';
-  lowerDisplay.textContent = lowerNumber;
-  upperDisplay.textContent = upperNumber;
+  window.location.reload();
+})
 
+function roundTo5decimalsMax(result){
+  result *= 100000;
+  result = Math.round(result);
+  return result / 100000;
+}
+
+clearButton.addEventListener("click",()=>{
+  lowerNumber = lowerNumber.slice(0,-1);
+  lowerDisplay.textContent = lowerNumber;
 })
